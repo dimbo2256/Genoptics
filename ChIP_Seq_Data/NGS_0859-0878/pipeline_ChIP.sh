81e923fd7ddcb482050f1060905ae4ea43292f13
Dimbo <dim.botskaris@gmail.com>
2021-02-10 14:52:03 +0200

    add

--- ChIP_Seq_Data/NGS_0859-0878/pipeline_ChIP.sh
+++ ChIP_Seq_Data/NGS_0859-0878/pipeline_ChIP.sh
@@ -5,6 +5,7 @@ for ((x = $1; x <= $2; x++))
 do
 	sample="0${x}.tesing"
 	summary="summary_0${x}.txt"
+	fastq_input="0${x}.fastq"
 	input_sam="0${x}.sam"
 	output_bam_tmp="0${x}.bam.tmp"
 	output_bam="0${x}.bam"
@@ -13,8 +14,8 @@ do
 echo "testing"
 echo $sample
 
-#mapping with hisat2
-hisat2 -p 7 --summary-file $summary -x ../../../../Genomes_Fasta/mus_Genome/hista2_index/mus_genome -U 0859.fastq.gz -S $input_sam
+# mapping with hisat2
+hisat2 -p 7 --summary-file $summary -x ../../../../Genomes_Fasta/mus_Genome/hista2_index/mus_genome -U $fastq_input -S $input_sam
 
 # keep only unique
 samtools view -h -F 4 $input_sam | awk 'substr($1, 0, 1)=="@" || $0 !~ /ZS:/' | samtools view -h -b > $output_bam_tmp
@@ -28,7 +29,7 @@ samtools sort $output_bam -o $output_sorted_bam
 # index bam
 samtools index $output_sorted_bam
 
-# remove files
+# remove tmp files
 rm $input_sam $output_bam.tmp $output_bam
 
 
